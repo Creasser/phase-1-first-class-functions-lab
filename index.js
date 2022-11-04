3573663e2f9065bc80f0ef566f7037af68588e46
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-03 20:16:23 -0500

    commit tree 983ecf70d420b3a604aaa1ada5ae9804a3adbaa8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(driver){
+    return driver.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (driver){
+    return driver.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+
+    return function (fare){
+       return fare * num
+   }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(driver, func){
+    return func(driver)
+}
